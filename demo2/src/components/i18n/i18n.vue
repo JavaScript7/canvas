fec51d0510c4a7cac941d217222dafafb100362c
林星发 <linxingfa@maoniunet.com>
2018-06-01 18:22:35 +0800

    i18n中文英文切换

---
+++ demo2/src/components/i18n/i18n.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="i18n">
+    <button @click="changeLanguage">切换中英文</button>
+    <h1 style="font-size: 16px; text-align: center;">
+      {{$t('messageLanguage.hello')}}
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'i18n',
+  data () {
+    return {}
+  },
+  methods: {
+    changeLanguage () {
+      console.log(this.i18n)
+    }
+  }
+}
+</script>
+<style>
+
+</style>
